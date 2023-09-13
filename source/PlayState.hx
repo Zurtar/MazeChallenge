d66975f2f94e4cf47a05b8c5b8a5f7fa343822de
Zurtar <ethan.cook3552@gmail.com>
2023-09-13 12:24:04 -0400

    Grid Test
    Just seeing if I can display a grid of my tiles! randomly chosen between pathway or wall.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,19 +1,51 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.FlxState;
+import flixel.math.FlxPoint;
+import flixel.path.FlxPath;
+import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxStringUtil;
+import haxe.Log;
+
+using flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+		add(createMap(5, 5));
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 	}
+
+	function createMap(width:Int, height:Int):FlxTilemap
+	{
+		var newMap:FlxTilemap = new FlxTilemap();
+		newMap = new FlxTilemap();
+
+		var gridCSV = FlxStringUtil.arrayToCSV([
+			for (i in 0...height * width)
+				FlxG.random.bool() ? 0 : 1
+		], width);
+
+		newMap.loadMapFromCSV(gridCSV, AssetPaths.tiles__png, 64, 64);
+
+		// Manualy set the collision properties of our different tile types.
+		newMap.setTileProperties(1, NONE);
+		newMap.setTileProperties(2, ANY);
+		newMap.setTileProperties(3, NONE);
+		newMap.setTileProperties(4, NONE);
+
+		return newMap;
+	}
 }
-/**
- * jasjdwj
- */
